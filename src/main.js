681436e7ea2ba524cdb493de6e97d61835fa3ba0
huangjunlong1314 <8498595+huangjunlong1314@user.noreply.gitee.com>
2021-04-03 20:14:27 +0800

    项目初始化 引入Vant 组件库

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import './styles/index.less'
+// 加载/导入 Vant 组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
-
+// 全局注册 使用 Vant组件库
+Vue.use(Vant)
 new Vue({
   router,
   store,
